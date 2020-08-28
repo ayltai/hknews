2c7bc79639f74987f5c2880cde0e4c6a629abe33
Alan Tai <ayltai@gmail.com>
2020-08-28 09:34:14 +0800

    Update news item layout; update dependencies

--- src/main/javascript/view/list/ItemGrid.jsx
+++ src/main/javascript/view/list/ItemGrid.jsx
@@ -13,30 +13,40 @@ export const ItemGrid = ({ basePath, ids, data, }) => {
             padding  : 16,
             flexGrow : 1,
         },
+        card        : {
+            display : 'flex',
+        },
+        details     : {
+            display       : 'flex',
+            flexDirection : 'column',
+        },
+        content     : {
+            flex : '1 0 auto',
+        },
         title       : {
-            paddingBottom : '0.75em',
-            display       : 'block',
-            overflow      : 'hidden',
-            fontSize      : '120%',
-            fontWeight    : 'bold',
-            textOverflow  : 'ellipsis',
-            whiteSpace    : 'nowrap',
+            display         : '-webkit-box',
+            overflow        : 'hidden',
+            fontSize        : '120%',
+            fontWeight      : 'bold',
+            textOverflow    : 'ellipsis',
+            whiteSpace      : 'break-spaces',
+            WebkitBoxOrient : 'vertical',
+            WebkitLineClamp : 1,
         },
         description : {
-            minHeight       : 200,
+            minHeight       : 100,
             display         : '-webkit-box',
             overflow        : 'hidden',
             textOverflow    : 'ellipsis',
             WebkitBoxOrient : 'vertical',
-            WebkitLineClamp : 8,
+            WebkitLineClamp : 4,
         },
         media       : {
-            height     : 0,
-            paddingTop : '56.25%',
+            minWidth : 300,
+            width    : 300,
         },
         actions     : {
-            display   : 'block',
-            textAlign : 'right',
+            display : 'block',
         },
     })();
 
@@ -49,52 +59,54 @@ export const ItemGrid = ({ basePath, ids, data, }) => {
                     <Grid
                         item
                         key={id}
-                        xs={12}
-                        sm={6}
-                        md={6}
-                        lg={4}
-                        xl={3}>
+                        lg={12}
+                        xl={6}>
                         <Card>
-                            <CardHeader
-                                title={
-                                    <TextField
-                                        record={data[id]}
-                                        source='sourceName' />
-                                }
-                                subheader={<TimeAgo date={data[id].publishDate} />}
-                                avatar={
-                                    <img
-                                        src={`${Constants.API_ENDPOINT}/images/${Constants.SOURCE_IMAGES[data[id].sourceName]}.png`}
-                                        width={Constants.AVATAR_SIZE}
-                                        height={Constants.AVATAR_SIZE}
-                                        title={data[id].sourceName}
-                                        alt={data[id].sourceName} />
-                                } />
                             <CardActionArea href={`#${basePath}/${id}/show`}>
-                                {data[id].images && data[id].images.length > 0 && (
-                                    <Tooltip title={data[id].images[0].description ? StringUtils.decode(data[id].images[0].description) : ''}>
-                                        <CardMedia
-                                            className={classes.media}
-                                            image={data[id].images[0].imageUrl} />
-                                    </Tooltip>
-                                )}
-                                <CardContent>
-                                    <RichTextField
-                                        className={classes.title}
-                                        record={data[id]}
-                                        source='title' />
-                                    <RichTextField
-                                        className={classes.description}
-                                        record={data[id]}
-                                        source='description' />
-                                </CardContent>
+                                <div className={classes.card}>
+                                    <div className={classes.details}>
+                                        <CardHeader
+                                            className={classes.content}
+                                            title={
+                                                <TextField
+                                                    record={data[id]}
+                                                    source='sourceName' />
+                                            }
+                                            subheader={<TimeAgo date={data[id].publishDate} />}
+                                            avatar={
+                                                <img
+                                                    src={`${Constants.API_ENDPOINT}/images/${Constants.SOURCE_IMAGES[data[id].sourceName]}.png`}
+                                                    width={Constants.AVATAR_SIZE}
+                                                    height={Constants.AVATAR_SIZE}
+                                                    title={data[id].sourceName}
+                                                    alt={data[id].sourceName} />
+                                            } />
+                                        <CardContent className={classes.content}>
+                                            <RichTextField
+                                                className={classes.title}
+                                                record={data[id]}
+                                                source='title' />
+                                            <RichTextField
+                                                className={classes.description}
+                                                record={data[id]}
+                                                source='description' />
+                                        </CardContent>
+                                        <CardActions className={classes.actions}>
+                                            <ShowButton
+                                                basePath={basePath}
+                                                label='labels.read_more'
+                                                record={data[id]} />
+                                        </CardActions>
+                                    </div>
+                                    {data[id].images && data[id].images.length > 0 && (
+                                        <Tooltip title={data[id].images[0].description ? StringUtils.decode(data[id].images[0].description) : ''}>
+                                            <CardMedia
+                                                className={classes.media}
+                                                image={data[id].images[0].imageUrl} />
+                                        </Tooltip>
+                                    )}
+                                </div>
                             </CardActionArea>
-                            <CardActions className={classes.actions}>
-                                <ShowButton
-                                    basePath={basePath}
-                                    label='labels.read_more'
-                                    record={data[id]} />
-                            </CardActions>
                         </Card>
                     </Grid>
                 ))}
