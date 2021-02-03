a432659850b7cad5c79f26759459badcb10a9ba2
Alan Tai <ayltai@gmail.com>
2021-02-03 16:52:02 +0800

    Reduce cron event frequency to save cost

--- src/main/terraform/functions.tf
+++ src/main/terraform/functions.tf
@@ -10,97 +10,97 @@ locals {
       function_name : "AppleDailyParser"
       handler : "com.github.ayltai.hknews.handler.AppleDailyParserHandler::handleRequest"
       event_name : "appledaily"
-      schedule_expression : "cron(0/20 * * * ? *)"
+      schedule_expression : "cron(0/30 * * * ? *)"
     },
     {
       function_name : "HeadlineParser"
       handler : "com.github.ayltai.hknews.handler.HeadlineParserHandler::handleRequest"
       event_name : "headline"
-      schedule_expression : "cron(1/20 * * * ? *)"
+      schedule_expression : "cron(1/30 * * * ? *)"
     },
     {
       function_name : "HeadlineRealtimeParser"
       handler : "com.github.ayltai.hknews.handler.HeadlineRealtimeParserHandler::handleRequest"
       event_name : "headline_realtime"
-      schedule_expression : "cron(2/20 * * * ? *)"
+      schedule_expression : "cron(2/30 * * * ? *)"
     },
     {
       function_name : "HkejParser"
       handler : "com.github.ayltai.hknews.handler.HkejParserHandler::handleRequest"
       event_name : "hkej"
-      schedule_expression : "cron(3/20 * * * ? *)"
+      schedule_expression : "cron(3/30 * * * ? *)"
     },
     {
       function_name : "HkejRealtimeParser"
       handler : "com.github.ayltai.hknews.handler.HkejRealtimeParserHandler::handleRequest"
       event_name : "hkej_realtime"
-      schedule_expression : "cron(4/20 * * * ? *)"
+      schedule_expression : "cron(4/30 * * * ? *)"
     },
     {
       function_name : "HketParser"
       handler : "com.github.ayltai.hknews.handler.HketParserHandler::handleRequest"
       event_name : "hket"
-      schedule_expression : "cron(5/20 * * * ? *)"
+      schedule_expression : "cron(5/30 * * * ? *)"
     },
     {
       function_name : "MingPaoParser"
       handler : "com.github.ayltai.hknews.handler.MingPaoParserHandler::handleRequest"
       event_name : "mingpao"
-      schedule_expression : "cron(6/20 * * * ? *)"
+      schedule_expression : "cron(6/30 * * * ? *)"
     },
     {
       function_name : "OrientalDailyParser"
       handler : "com.github.ayltai.hknews.handler.OrientalDailyParserHandler::handleRequest"
       event_name : "orientaldaily"
-      schedule_expression : "cron(7/20 * * * ? *)"
+      schedule_expression : "cron(7/30 * * * ? *)"
     },
     {
       function_name : "OrientalDailyRealtimeParser"
       handler : "com.github.ayltai.hknews.handler.OrientalDailyRealtimeParserHandler::handleRequest"
       event_name : "orientaldaily_realtime"
-      schedule_expression : "cron(8/20 * * * ? *)"
+      schedule_expression : "cron(8/30 * * * ? *)"
     },
     {
       function_name : "RthkParser"
       handler : "com.github.ayltai.hknews.handler.RthkParserHandler::handleRequest"
       event_name : "rthk"
-      schedule_expression : "cron(9/20 * * * ? *)"
+      schedule_expression : "cron(9/30 * * * ? *)"
     },
     {
       function_name : "ScmpParser"
       handler : "com.github.ayltai.hknews.handler.ScmpParserHandler::handleRequest"
       event_name : "scmp"
-      schedule_expression : "cron(10/20 * * * ? *)"
+      schedule_expression : "cron(10/30 * * * ? *)"
     },
     {
       function_name : "SingPaoParser"
       handler : "com.github.ayltai.hknews.handler.SingPaoParserHandler::handleRequest"
       event_name : "singpao"
-      schedule_expression : "cron(11/20 * * * ? *)"
+      schedule_expression : "cron(11/30 * * * ? *)"
     },
     {
       function_name : "SingTaoParser"
       handler : "com.github.ayltai.hknews.handler.SingTaoParserHandler::handleRequest"
       event_name : "singtao"
-      schedule_expression : "cron(12/20 * * * ? *)"
+      schedule_expression : "cron(12/30 * * * ? *)"
     },
     {
       function_name : "SkyPostParser"
       handler : "com.github.ayltai.hknews.handler.SkyPostParserHandler::handleRequest"
       event_name : "skypost"
-      schedule_expression : "cron(13/20 * * * ? *)"
+      schedule_expression : "cron(13/30 * * * ? *)"
     },
     {
       function_name : "TheStandardParser"
       handler : "com.github.ayltai.hknews.handler.TheStandardParserHandler::handleRequest"
       event_name : "thestandard"
-      schedule_expression : "cron(14/20 * * * ? *)"
+      schedule_expression : "cron(14/30 * * * ? *)"
     },
     {
       function_name : "WenWeiPoParser"
       handler : "com.github.ayltai.hknews.handler.WenWeiPoParserHandler::handleRequest"
       event_name : "wenweipo"
-      schedule_expression : "cron(15/20 * * * ? *)"
+      schedule_expression : "cron(15/30 * * * ? *)"
     },
   ]
 }
