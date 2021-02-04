a09ed9add94d33ecb750cd7a3086bb6be4fc905d
Alan Tai <ayltai@gmail.com>
2021-02-04 11:55:12 +0800

    Use API Gateway mock response to handle root ("/") requests

--- src/main/terraform/networks.tf
+++ src/main/terraform/networks.tf
@@ -34,12 +34,62 @@ resource "aws_api_gateway_method_settings" "this" {
   }
 }
 
+resource "aws_api_gateway_method" "root" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_rest_api.this.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "root" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_rest_api.this.root_resource_id
+  http_method = aws_api_gateway_method.root.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "root" {
+  rest_api_id          = aws_api_gateway_rest_api.this.id
+  resource_id          = aws_api_gateway_rest_api.this.root_resource_id
+  http_method          = aws_api_gateway_method.root.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "root" {
+  rest_api_id        = aws_api_gateway_rest_api.this.id
+  resource_id        = aws_api_gateway_rest_api.this.root_resource_id
+  http_method        = aws_api_gateway_method.root.http_method
+  status_code        = aws_api_gateway_method_response.root.status_code
+  response_templates = aws_api_gateway_integration.root.request_templates
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,HEAD,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.root,
+  ]
+}
+
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = var.api_stage_name
 
   triggers = {
-    redeployment = sha1(join(",", list(jsonencode(module.lambda_source), jsonencode(module.lambda_item), jsonencode(module.lambda_items))))
+    redeployment = sha1(join(",", list(jsonencode(module.lambda_source), jsonencode(module.lambda_item), jsonencode(module.lambda_items), jsonencode(aws_api_gateway_integration.root))))
   }
 
   lifecycle {
@@ -50,6 +100,8 @@ resource "aws_api_gateway_deployment" "this" {
     module.lambda_source,
     module.lambda_item,
     module.lambda_items,
+    aws_api_gateway_integration.root,
+    aws_api_gateway_integration_response.root,
   ]
 }
 
