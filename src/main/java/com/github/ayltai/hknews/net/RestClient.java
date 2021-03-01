228323974bb5d2f8a0c1e108124bfc562451f9d6
Alan Tai <ayltai@gmail.com>
2021-03-01 10:54:25 +0800

    Fix URL encoding bug

--- src/main/java/com/github/ayltai/hknews/net/RestClient.java
+++ src/main/java/com/github/ayltai/hknews/net/RestClient.java
@@ -1,9 +1,6 @@
 package com.github.ayltai.hknews.net;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -14,7 +11,6 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.github.ayltai.hknews.Configuration;
 import com.github.ayltai.hknews.data.model.rss.Root;
 import com.google.gson.Gson;
-
 import lombok.experimental.UtilityClass;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
@@ -65,15 +61,15 @@ public class RestClient {
         .build();
 
     public String get(@NotNull final String url) throws IOException {
-        return RestClient.request(new HttpGet(RestClient.escapeUrl(url)), null);
+        return RestClient.request(new HttpGet(url), null);
     }
 
     public <T> T get(@NotNull final String url, @NotNull final Class<T> clazz) throws IOException {
-        return RestClient.request(new HttpGet(RestClient.escapeUrl(url)), clazz);
+        return RestClient.request(new HttpGet(url), clazz);
     }
 
     public <T> T post(@NotNull final String url, @NotNull final Map<String, String> fields) throws IOException {
-        final HttpPost request = new HttpPost(RestClient.escapeUrl(url));
+        final HttpPost request = new HttpPost(url);
         request.setEntity(new UrlEncodedFormEntity(fields.entrySet().stream().map(field -> new BasicNameValuePair(field.getKey(), field.getValue())).collect(Collectors.toList())));
 
         return RestClient.request(request, null);
@@ -84,17 +80,6 @@ public class RestClient {
             if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) return clazz == null ? (T)EntityUtils.toString(response.getEntity(), RestClient.WEN_WEI_PO_ENDPOINT.equals(request.getURI().getHost()) ? RestClient.CHARSET_BIG5 : StandardCharsets.UTF_8) : clazz.isAssignableFrom(Root.class) ? new XmlMapper().readValue(EntityUtils.toString(response.getEntity(), RestClient.WEN_WEI_PO_ENDPOINT.equals(request.getURI().getHost()) ? RestClient.CHARSET_BIG5 : StandardCharsets.UTF_8), clazz) : RestClient.GSON.fromJson(EntityUtils.toString(response.getEntity(), RestClient.WEN_WEI_PO_ENDPOINT.equals(request.getURI().getHost()) ? RestClient.CHARSET_BIG5 : StandardCharsets.UTF_8), clazz);
 
             throw new ClientProtocolException("Received HTTP " + response.getStatusLine().getStatusCode());
-        }
-    }
-
-    @NotNull
-    private static String escapeUrl(@NotNull final String url) throws IOException {
-        final URL uri = new URL(url);
-
-        try {
-            return new URI(uri.getProtocol(), uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(), uri.getRef()).toASCIIString();
-        } catch (final URISyntaxException e) {
-            throw new IOException(e);
         }
     }
 }
